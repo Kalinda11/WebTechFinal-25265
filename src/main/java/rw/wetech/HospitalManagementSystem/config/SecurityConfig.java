b1cd20d2d636e466adaf40cc05d6d1632cc7100f
Kalinda Benjamin <your_email@example.com>
2024-12-05 17:36:06 +0200

    refractored application properties

--- src/main/java/rw/wetech/HospitalManagementSystem/config/SecurityConfig.java
+++ src/main/java/rw/wetech/HospitalManagementSystem/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5174"));
+        configuration.addAllowedOrigin("https://hospitalmis.vercel.app");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
